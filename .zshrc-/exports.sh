4052822134d0c0c459c35e19da59b69e599feda6
Michael Scott Hertzberg <mshertzberg@gmail.com>
2014-12-13 17:26:56 -0500

    some recent updates

--- .zshrc-/exports.sh
+++ .zshrc-/exports.sh
@@ -1,11 +1,43 @@
 # ZSH
+plugins=''
+
+if [ ! -d $ZSH_CUSTOM/plugins/hipchat ]; then
+  HIPCHAT_API_TOKEN=""
+  git clone git@github.com:robertzk/hipchat.zsh.git $ZSH_CUSTOM/plugins/hipchat
+fi
+
+if [ ! -d $ZSH_CUSTOM/plugins/caniuse ]; then
+  git clone https://github.com/walesmd/caniuse.plugin.zsh $ZSH_CUSTOM/plugins/caniuse
+
+fi
+
+if [ ! -d $ZSH_CUSTOM/plugins/grunt ]; then
+  git clone git@github.com:clauswitt/zsh-grunt-plugin.git $ZSH_CUSTOM/plugins/grunt
+fi
+
+if [ ! -d $ZSH_CUSTOM/plugins/git-extra-commands ]; then
+  git clone https://github.com/unixorn/git-extra-commands.git $ZSH_CUSTOM/plugins/git-extra-commands
+fi
+
+if [ ! -d $ZSH_CUSTOM/plugins/alias-tips ]; then
+  git clone https://github.com/djui/alias-tips.git $ZSH_CUSTOM/plugins/alias-tips
+fi
+
 ZSH=$HOME/.oh-my-zsh
 #ZSH_THEME='nanotech'
 ZSH_THEME='nicoulaj'
 COMPLETION_WAITING_DOTS=true
 DISABLE_UPDATE_PROMPT=true
-# plugins=(git-extras grunt urltools npm bower sync)
-plugins=(bundler gitfast git-extras npm grunt caniuse)
+
+plugins+=(bundler)
+plugins+=(gitfast)
+plugins+=(git-extras)
+plugins+=(npm)
+plugins+=(git-extra-commands)
+plugins+=(grunt)
+plugins+=(caniuse)
+plugins+=(hipchat)
+plugins+=(alias-tips)
 
 EDITOR='subl -w'
 ARCHFLAGS='-arch x86_64'
