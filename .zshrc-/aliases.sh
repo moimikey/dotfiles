9f8abc65292a541d41016215c4c4993e4f52b98f
Michael Scott Hertzberg <mshertzberg@gmail.com>
2014-12-15 21:39:27 -0500

    oof. fixups!

--- .zshrc-/aliases.sh
+++ .zshrc-/aliases.sh
@@ -1,24 +1,24 @@
 # Override
-alias wget="wget -c"
+alias ack='ag -u --stats'
+alias bower='noglob bower'
 alias grunt="noglob grunt"
+alias node='node --harmony'
+alias php='/usr/bin/php'
 alias port="port -d"
 alias vi="vim"
-alias bower='noglob bower'
-alias php='/usr/bin/php'
-alias ack='ag -u --stats'
-alias node='node --harmony'
+alias wget="wget -c"
 
 # ImageMagick
+alias blackandwhite='mogrify -colorspace gray'
 alias transparentize='mogrify -transparent white'
 alias whataretheseimages='for file in *.(JPG|jpg|gif|GIF|jpeg|JPEG); do identify -format "filename: %f | filesize: %[size] | orien: %[orientation] \n" $file; done'
-alias blackandwhite='mogrify -colorspace gray'
 
 # Image related...
 alias b64="xargs -I % bash -c 'openssl base64 < %' <<<"
 
 # Composer
-alias getcomposer="curl -s https://getcomposer.org/installer | php"
 alias composer="php composer.phar"
+alias getcomposer="curl -s https://getcomposer.org/installer | php"
 
 # Git
 alias howbigismycodebase="git ls-files | grep coffee | xargs wc -l"
@@ -26,43 +26,43 @@ alias pull="gf && xargs -I % bash -c 'git checkout pr/%' <<<"
 
 # Util
 alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl"
-alias show="defaults write com.apple.Finder AppleShowAllFiles -bool true && killall Finder"
-alias hide="defaults write com.apple.Finder AppleShowAllFiles -bool false && killall Finder"
-alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
-alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
-alias root="_ -i"
+alias flush="sudo dscacheutil -flushcache; echo `ps aux | grep mDNSResponder | grep -v grep | awk '{print $2}'` | xargs sudo kill -HUP"
 alias flushall="echo 'flush_all' | nc localhost 11211 -i1 <<< 'quit'"
 alias getcomposer="curl -s https://getcomposer.org/installer | php"
-alias klear="find . -maxdepth 1 -delete"
-alias json="python -mjson.tool"
-alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1]);"'
-alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
-alias sniff="sudo ngrep -d 'en0' -t '^(GET|POST) ' 'tcp and port 80'"
-alias pub="more ~/.ssh/id_rsa.pub | pbcopy | echo 'public key copied to clipboard'"
-alias flush="sudo dscacheutil -flushcache; echo `ps aux | grep mDNSResponder | grep -v grep | awk '{print $2}'` | xargs sudo kill -HUP"
+alias hide="defaults write com.apple.Finder AppleShowAllFiles -bool false && killall Finder"
+alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
 alias hosts="_ vi /etc/hosts"
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias json="python -mjson.tool"
+alias klear="find . -maxdepth 1 -delete"
+alias pub="more ~/.ssh/id_rsa.pub | pbcopy | echo 'public key copied to clipboard'"
+alias root="_ -i"
+alias show="defaults write com.apple.Finder AppleShowAllFiles -bool true && killall Finder"
+alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
+alias sniff="sudo ngrep -d 'en0' -t '^(GET|POST) ' 'tcp and port 80'"
+alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1]);"'
 
 # Fixes
-alias fixcamera="sudo killall VDCAssistant"
 alias fixaudio="sudo killall coreaudiod"
-alias fixnpm="sudo npm -g cache clean"
+alias fixcamera="sudo killall VDCAssistant"
 alias fixdupes="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -all s,l,u; killall Finder"
+alias fixnpm="sudo npm -g cache clean"
 
 # Fun
-alias wiki="xargs -I % bash -c 'dig +short txt %.wp.dg.cx' <<<"
-alias up="/Users/mshertzberg/.scripts/Dropbox-Uploader/dropbox_uploader.sh"
 alias lol="lolcommits --enable"
+alias up="/Users/mshertzberg/.scripts/Dropbox-Uploader/dropbox_uploader.sh"
+alias wiki="xargs -I % bash -c 'dig +short txt %.wp.dg.cx' <<<"
 
 # Browse
-alias safari="open -a safari"
+alias canary-nosec="open /Applications/Google\ Chrome\ Canary.app --args --disable-web-security --disable-prompt-on-repost"
+alias canary="open -a /Applications/Google\ Chrome\ Canary.app"
+alias chrome-nosec="open /Applications/Google\ Chrome.app --args --disable-web-security --disable-prompt-on-repost --allow-running-insecure-content --ignore-certificate-errors"
+alias chrome="open -a /Applications/Google\ Chrome.app"
 alias firefox="open -a firefox"
 alias opera="open -a opera"
-alias chrome="open -a /Applications/Google\ Chrome.app"
-alias chrome-nosec="open /Applications/Google\ Chrome.app --args --disable-web-security --disable-prompt-on-repost --allow-running-insecure-content --ignore-certificate-errors"
-alias canary="open -a /Applications/Google\ Chrome\ Canary.app"
-alias canary-nosec="open /Applications/Google\ Chrome\ Canary.app --args --disable-web-security --disable-prompt-on-repost"
+alias safari="open -a safari"
 
 # ZSH
-alias zshconfig="vi ~/.zshrc"
 alias ohmyzsh="mate ~/.oh-my-zsh"
 alias reload="source ~/.zshrc"
+alias zshconfig="vi ~/.zshrc"
--- .zshrc-/exports.sh
+++ .zshrc-/exports.sh
@@ -1,70 +1,42 @@
 # ZSH
 plugins=''
 
-if [ ! -d $ZSH_CUSTOM/plugins/hipchat ]; then
-  HIPCHAT_API_TOKEN=""
-  git clone git@github.com:robertzk/hipchat.zsh.git $ZSH_CUSTOM/plugins/hipchat
-fi
-
-if [ ! -d $ZSH_CUSTOM/plugins/caniuse ]; then
-  git clone https://github.com/walesmd/caniuse.plugin.zsh $ZSH_CUSTOM/plugins/caniuse
-fi
-
-if [ ! -d $ZSH_CUSTOM/plugins/grunt ]; then
-  git clone git@github.com:clauswitt/zsh-grunt-plugin.git $ZSH_CUSTOM/plugins/grunt
-fi
-
-if [ ! -d $ZSH_CUSTOM/plugins/git-extra-commands ]; then
-  git clone https://github.com/unixorn/git-extra-commands.git $ZSH_CUSTOM/plugins/git-extra-commands
-fi
-
-if [ ! -d $ZSH_CUSTOM/plugins/alias-tips ]; then
-  git clone https://github.com/djui/alias-tips.git $ZSH_CUSTOM/plugins/alias-tips
-fi
-
 ZSH=$HOME/.oh-my-zsh
 #ZSH_THEME='nanotech'
 ZSH_THEME='nicoulaj'
 COMPLETION_WAITING_DOTS=true
 DISABLE_UPDATE_PROMPT=true
 
-plugins+=(bundler)
-plugins+=(gitfast)
-plugins+=(git-extras)
-plugins+=(npm)
-plugins+=(git-extra-commands)
-plugins+=(grunt)
-plugins+=(caniuse)
-plugins+=(hipchat)
+if [ ! -d ~/.oh-my-zsh/custom/plugins/hipchat ]; then
+  HIPCHAT_API_TOKEN=""
+  git clone git@github.com:robertzk/hipchat.zsh.git ~/.oh-my-zsh/custom/plugins/hipchat
+fi
+
+if [ ! -d ~/.oh-my-zsh/custom/plugins/caniuse ]; then
+  git clone https://github.com/walesmd/caniuse.plugin.zsh ~/.oh-my-zsh/custom/plugins/caniuse
+fi
+
+if [ ! -d ~/.oh-my-zsh/custom/plugins/grunt ]; then
+  git clone git@github.com:clauswitt/zsh-grunt-plugin.git ~/.oh-my-zsh/custom/plugins/grunt
+fi
+
+if [ ! -d ~/.oh-my-zsh/custom/plugins/git-extra-commands ]; then
+  git clone https://github.com/unixorn/git-extra-commands.git ~/.oh-my-zsh/custom/plugins/git-extra-commands
+fi
+
+if [ ! -d ~/.oh-my-zsh/custom/plugins/alias-tips ]; then
+ git clone https://github.com/djui/alias-tips.git ~/.oh-my-zsh/custom/plugins/alias-tips
+fi
+
 plugins+=(alias-tips)
+plugins+=(bundler)
+plugins+=(caniuse)
+plugins+=(git-extra-commands)
+plugins+=(git-extras)
+plugins+=(gitfast)
+plugins+=(grunt)
+plugins+=(hipchat)
+plugins+=(npm)
 
 EDITOR='subl -w'
 ARCHFLAGS='-arch x86_64'
-
-# git changelog [-l/--list] - populate changelog file with commits since the previous tag
-# git contrib - display author contributions
-# git count [--all] - count commits
-# git delete-branch - delete local and remote branch
-# git delete-submodule - delete submodule
-# git delete-tag - delete local and remote tag
-# git extras [-v/--version] - git-extras
-# git graft - merge commits from source branch to destination branch
-# git squash - merge commits from source branch into the current one as a single commit
-# git feature [finish] - create a feature branch
-# git refactor [finish] - create a refactor branch
-# git bug [finish] - create a bug branch
-# git summary - repository summary
-# git effort [--above] - display effort statistics
-# git repl - read-eval-print-loop
-# git commits-since - list commits since a given date
-# git release - release commit with the given tag
-# git alias - define, search and show aliases
-# git ignore - add patterns to .gitignore
-# git info - show info about the repository
-# git create-branch - create local and remote branch
-# git fresh-branch - create empty local branch
-# git undo - remove the latest commit
-# git setup - setup a git repository
-# git touch - one step creation of new files
-# git obliterate - Completely remove a file from the repository, including past commits and tags
-# git local-commits - list unpushed commits on the local branch
