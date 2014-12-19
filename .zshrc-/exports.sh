ba9f933a470d1a212941a2032e7b83bb8c1710b0
Michael Scott Hertzberg <mshertzberg@gmail.com>
2014-12-18 23:29:19 -0500

    maximize that histsize

--- .zshrc-/exports.sh
+++ .zshrc-/exports.sh
@@ -6,6 +6,7 @@ ZSH=$HOME/.oh-my-zsh
 ZSH_THEME='nicoulaj'
 COMPLETION_WAITING_DOTS=true
 DISABLE_UPDATE_PROMPT=true
+HISTSIZE=99999999
 
 if [ ! -d ~/.oh-my-zsh/custom/plugins/hipchat ]; then
   HIPCHAT_API_TOKEN=""
